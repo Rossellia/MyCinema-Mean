02a712454fb68d8f4585842fe3c0e9275fd3ca2b
Aziatx <barbualexandru14@yahoo.com>
2019-04-16 00:14:03 +0300

    Meniu added

--- angular-statistics/src/app/app.module.ts
+++ angular-statistics/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { PieChartComponent } from './piecharts/piecharts.component';
 import { AgmCoreModule } from '@agm/core';
 import { DisplayPiechartsComponent } from './display-piecharts/display-piecharts.component';
 import { GooglePieChartService } from './google-charts.service.spec';
+import { HomebarComponent } from './homebar/homebar.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
+import { UserbarComponent } from './userbar/userbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarchartsComponent,
     PieChartComponent,
-    DisplayPiechartsComponent
+    DisplayPiechartsComponent,
+    HomebarComponent,
+    HeaderbarComponent,
+    UserbarComponent
   ],
   imports: [
     BrowserModule,
