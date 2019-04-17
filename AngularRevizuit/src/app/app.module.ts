a54bc01e84e5ab52bd14d64a24fa8ca7b3ae080e
Rossellia <raluca.bucnaru@gmail.com>
2019-04-17 17:22:44 +0300

    RefacereAngular+Routing

---
+++ AngularRevizuit/src/app/app.module.ts
@@ -0,0 +1,69 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomebarComponent } from './homebar/homebar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { CinemaroomsComponent } from './cinemarooms/cinemarooms.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ProgramComponent } from './program/program.component';
+import { RoomComponent } from './room/room.component';
+import { SettingsComponent } from './settings/settings.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ScriptsComponent } from './scripts/scripts.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { APP_BASE_HREF } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomebarComponent,
+    SearchbarComponent,
+    CinemaroomsComponent,
+    HomeComponent,
+    MoviesComponent,
+    ProgramComponent,
+    RoomComponent,
+    SettingsComponent,
+    StatisticsComponent,
+    ScriptsComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'home', component: HomeComponent
+      },
+      {
+        path: 'movies', component: MoviesComponent
+      },
+      {
+        path: 'program', component: ProgramComponent
+      },
+      {
+        path: 'cinemarooms/:room', component: RoomComponent
+      },
+      {
+        path: 'cinemarooms', component: CinemaroomsComponent
+      },
+
+      {
+        path: 'settings', component: SettingsComponent
+      },
+      {
+        path: 'statistics', component: StatisticsComponent
+      }
+    ])
+
+  ],
+  providers: [ {provide: APP_BASE_HREF, useValue: '/' }],
+  exports: [RouterModule] ,
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularRevizuit/src/app/cinemarooms/cinemarooms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cinemarooms',
+  templateUrl: './cinemarooms.component.html',
+  styleUrls: ['./cinemarooms.component.css' ]
+})
+export class CinemaroomsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
